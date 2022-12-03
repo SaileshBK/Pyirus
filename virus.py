0fd2897dd7e6e806861520b67299e438b7f35b8e
SaileshBK <sailesh.bk1@icloud.com>
2022-12-02 23:36:25 -0600

    Using for loop instead while

--- virus.py
+++ virus.py
@@ -3,27 +3,22 @@ import turtle
 wn = turtle.Screen()
 wn.bgcolor('black')
 turtle.speed(600)
-x = 200
-while x > 0:
+for x in range(200, 0, -1):
     if (x % 2) == 0:
         turtle.color('cyan')
     else:
         turtle.color('green')
     turtle.left(x)
     turtle.forward(x * 2)
-    x = x - 1
 turtle.up()
-turtle.left(75)    
+turtle.left(75)
 turtle.forward(700)
 turtle.down()
-x = 200
-while x > 0:
+for x in range(200, 0, -1):
     if (x % 2) == 0:
         turtle.color('green')
     else:
         turtle.color('cyan')
     turtle.left(x)
     turtle.forward(x * 2)
-    x = x - 1
-
 wn.exitonclick()
